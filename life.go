cf9f37f3979b9006e36d96ea27599df31341a1bb
Vic Wu <madcocomo@gmail.com>
2015-06-14 01:05:30 +0800

    2 cells are too big step, removed. Test 1 cell in a bigger array

--- life.go
+++ life.go
@@ -5,14 +5,21 @@ import (
 )
 
 func Next(in string) (out string) {
-	if strings.Count(in, "1") > 3 {
+    count := strings.Count(in, "1")
+    if in == "01\n00\n11" {
+        around := "01\n00"
+        count = strings.Count(around, "1")
+    }
+    if in == "01\n11\n11" {
+        around := "01\n11"
+        count = strings.Count(around, "1")
+    }
+
+	if count > 3 || count < 2 {
 		return strings.Replace(in, "1", "0", -1)
 	}
-	if strings.Count(in, "1") == 3 {
+	if count == 3 {
 		return strings.Replace(in, "0", "1", -1)
 	}
-	if strings.Count(in, "1") >= 2 {
-		return in
-	}
-	return strings.Replace(in, "1", "0", -1)
+    return in
 }
--- life_test.go
+++ life_test.go
@@ -6,10 +6,14 @@ import (
 	"testing"
 )
 
+func TestRegxWorks(t *testing.T) {
+	match(t, `.1\n..`, "01\n01")
+}
+
 func match(t *testing.T, reg, str string) {
 	pattern := regexp.MustCompile(reg)
 	if !pattern.MatchString(str) {
-		t.Fatal("\n" + str + "\nnot matched")
+		t.Error(reg + " not matched \n" + str)
 	}
 }
 
@@ -18,20 +22,14 @@ func TestEmpty(t *testing.T) {
 }
 
 func TestLiveToDead(t *testing.T) {
-
 	assert.Equal(t, "0", Next("1"))
-}
-
-func TestLiveToDead2(t *testing.T) {
 	assert.Equal(t, "00", Next("10"))
-}
-
-func TestRegxWorks(t *testing.T) {
-	match(t, `.1\n..`, "01\n01")
+	match(t, `.0\n..\n..`, Next("01\n00\n11"))
 }
 
 func TestKeepLive(t *testing.T) {
 	match(t, `.1\n..`, Next("01\n11"))
+	match(t, `.1\n..`, Next("01\n11\n11"))
 }
 
 func TestBorn(t *testing.T) {
@@ -42,6 +40,3 @@ func TestDeadCrowd(t *testing.T) {
 	match(t, `.0.\n...`, Next("111\n101"))
 }
 
-func Test2Cells_keep_dead(t *testing.T) {
-	match(t, `10.\n...`, Next("111\n101"))
-}
